398e82920fb5b22cbc262e45a557a23ed681fcee
manuelOttlik <manuel.ottlik@web.de>
2018-11-01 15:36:36 +0100

    example art presentation

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -2,26 +2,26 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
 use App\Artwork;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the presentation.
-     *
-     * @return \Illuminate\Http\Response
-     */
+/**
+ * Show the presentation.
+ *
+ * @return \Illuminate\Http\Response
+ */
     public function index()
     {
         return view('home', ['artworks' => Artwork::all()]);
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
+/**
+ * Show the application dashboard.
+ *
+ * @return \Illuminate\Http\Response
+ */
     public function dashboard()
     {
         return view('dashboard');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app') 
-@section('content') @foreach ($artworks as $artwork)
-<div class="card">
-    <div class="card-header">{{$artwork->name}} von {{$artwork->artist->name}}</div>
-    <div class="card-body">
-        <img src="{{Storage::url($artwork->file)}}" />
-    </div>
-    <div class="card-footer">
-        <table class="table">
-            <tr>
-                <th>1</th>
-                <td>2</td>
-            </tr>
-        </table>
-    </div>
-</div><br> @endforeach
+@section('content')
+<div class="d-flex justify-content-around align-items-center flex-wrap">
+
+@php
+    function random_color_part() {
+        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+    }
+    function random_color() {
+        return random_color_part() . random_color_part() . random_color_part();
+    } 
+@endphp
+
+@foreach ($artworks as $artwork)
+    <img src="https://via.placeholder.com/{{rand(200, 600)}}x{{rand(200, 600)}}/{{random_color()}}" alt="{{$artwork->name}}" style="width: 30%; padding: 10px;">
+@endforeach
 @endsection
